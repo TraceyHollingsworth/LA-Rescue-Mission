5e208ba000735380283bfc99408bd52ab2cea68a
TraceyHollingsworth <tracey.hollingsworth@gmail.com>
2014-12-15 13:05:39 -0500

    initial commit and with initial database migrations

---
+++ db/migrate/20141215180258_create_answers.rb
@@ -0,0 +1,9 @@
+class CreateAnswers < ActiveRecord::Migration
+  def change
+    create_table :answers do |t|
+      t.integer :user_id, null: false
+      t.integer :question_id, null: false
+      t.text :description, null: false
+    end
+  end
+end
