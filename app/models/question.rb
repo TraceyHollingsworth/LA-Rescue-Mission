1413ba754287dc95a82fe948e1afc60cb4866882
TraceyHollingsworth <tracey.hollingsworth@gmail.com>
2014-12-15 21:41:21 -0500

    undid simpleform

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,6 @@ class Question < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 40 }
   validates :description, presence: true, length: { minimum: 150,
     too_short: "%{count} characters is the minimum allowed" }
+
+  belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"
@@ -14,6 +13,8 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   resources :questions
+  resources :users
+
 
   # Example resource route with options:
   #   resources :products do
